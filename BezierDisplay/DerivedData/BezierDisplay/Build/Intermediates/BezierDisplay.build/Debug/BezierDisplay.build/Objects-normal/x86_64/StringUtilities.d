e20d3be189d43a911292dfd4c1fda264552f2335
Papoj Thamjaroenporn <episer@gmail.com>
2012-12-20 20:56:59 -0500

    Sync branch

--- BezierDisplay/DerivedData/BezierDisplay/Build/Intermediates/BezierDisplay.build/Debug/BezierDisplay.build/Objects-normal/x86_64/StringUtilities.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Papoj/Hua_Columbia/2012/1-Fall/Classes/W4901_Research_Changxi/VTS_Repository/BezierDisplay/BezierDisplay/StringUtilities.cpp \
-  /Users/Papoj/Hua_Columbia/2012/1-Fall/Classes/W4901_Research_Changxi/VTS_Repository/BezierDisplay/BezierDisplay/StringUtilities.h
